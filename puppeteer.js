cf63a027fd8cc07f0795c09c266b8007e0c5062a
Brendan Kane <brendanekane@gmail.com>
2023-03-10 15:10:12 -0500

    testing mutationobserver

--- puppeteer.js
+++ puppeteer.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 // /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222 --no-first-run --no-default-browser-check --user-data-dir=$(mktemp -d -t 'chrome-remote_data_dir')
 
 const chromeEndpoint =
-  'ws://127.0.0.1:9222/devtools/browser/35bf7362-6027-4090-aab7-963deddcb9e5';
+  'ws://127.0.0.1:9222/devtools/browser/f571fae6-5c7a-433d-8b98-d196195f8a71';
 
 const chromeOptions = {
   browserWSEndpoint: chromeEndpoint,
@@ -18,26 +18,28 @@ async function launchPage() {
   const browser = await puppeteer.connect(chromeOptions);
   const cashortrade = await browser.newPage();
 
-  await cashortrade.goto(
-    'https://cashortrade.org/goose-tickets/?performerID=26048&filter%5B%5D=sale&keywordSearch=&eventStart=&eventEnd=&venue_states%5B%5D=NY'
-  );
+  // await cashortrade.goto(
+  //   'https://cashortrade.org/goose-tickets/?performerID=26048&filter%5B%5D=sale&keywordSearch=&eventStart=&eventEnd=&venue_states%5B%5D=NY'
+  // );
+  await cashortrade.goto('https://cashortrade.org/tickets');
 
   await cashortrade.exposeFunction('puppeteerLogMutation', () => {
     console.log('Mutation Detected: A child node has been added or removed.');
   });
 
-  await cashortrade.evaluate(() => {
-    const target = document.querySelector('#postlist');
-    const observer = new MutationObserver((mutations) => {
-      for (const mutation of mutations) {
-        if (mutation.type === 'childList') {
-          puppeteerLogMutation();
-        }
-      }
-    });
-    observer.observe(target, { childList: true });
-  });
-  getPost(cashortrade);
+  // await cashortrade.evaluate(() => {
+  //   const target = document.querySelector('#postlist');
+  //   const observer = new MutationObserver((mutations) => {
+  //     for (const mutation of mutations) {
+  //       if (mutation.type === 'childList') {
+  //         puppeteerLogMutation();
+  //         console.log(mutation);
+  //       }
+  //     }
+  //   });
+  //   observer.observe(target, { childList: true });
+  // });
+  // getPost(cashortrade);
 }
 
 async function getPost(cashortrade) {
